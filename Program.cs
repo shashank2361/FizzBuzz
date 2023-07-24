957833116e9e2322a677c8ec2637364453c898ef
Shashank2362 <shashank2362@yahoo.co.uk>
2023-07-24 22:53:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+IFizzBuzzService fizzBuzzService = new FizzBuzzService();
+
+for (int i = 1; i <= 100; i++)
+{
+    string result = fizzBuzzService.GetFizzBuzz(i);
+    Console.WriteLine(result);
+}
+
+Console.ReadLine();
